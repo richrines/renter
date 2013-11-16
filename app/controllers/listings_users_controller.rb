a1c7eb7402f163b163bf17a2acfb851842d2198a
Rich Rines <rich.rines@me.com>
2013-11-16 15:35:33 -0800

    wip redux

--- app/controllers/listings_users_controller.rb
+++ app/controllers/listings_users_controller.rb
@@ -1,2 +1,11 @@
 class ListingsUsersController < ActionController::Base
+  def create
+    render text: 'success'
+  end
+
+  private
+
+  def listings_user_params
+    params.require(:listings_users).permit(:user_id, :listing_id, :liked, :viewed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Renter::Application.routes.draw do
   resources :listings, only: [:index]
+  resources :listings_users, only: [:create]
 end
